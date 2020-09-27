12baecfc9d46107d242bef8cc90fb2aa7ff72395
piotr dominiak <pdominia@metal.agh.edu.pl>
2020-09-27 20:06:51 +0200

    Add project files.

---
+++ LinqToXML/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Xml.Linq;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Xml;
+using System.Data.SqlTypes;
+
+namespace LinqToXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* XDocument xmlDocument = new XDocument(
+                     new XDeclaration("1.0", "utf-8", "yes"),
+                     new XComment("Test comment"),
+                     new XElement("SRX_coefficients",
+                         new XElement("TemperatureStart",1100),
+                         new XElement("G", 59701475138),
+                         new XElement("Mi_Constant", 8e10)));
+
+             xmlDocument.Save(@"Dat1312a.xml");
+
+             */
+
+            /*   XDocument xmlDocument = new XDocument(
+                   new XDeclaration("1.0", "utf-8", "yes"),
+                   new XComment("Test comment"),
+                   new XElement("Students",
+
+                       from student in Student.GetAllStudens()
+                       select new XElement("Student", new XAttribute("Id", student.Id),
+                                  new XElement("Name",student.Name),
+                                  new XElement("Gender", student.Gender),
+                                  new XElement("TotalMarks", student.TotalMarks)             
+                   )));
+               xmlDocument.Save(@"NewDataataaaa.xml");
+
+
+
+
+            XDocument data = XDocument.Load(@"E:\PracaInzynierska\PracaInzynierska\PracaInzynierska\Data\sampleXML_1.xml");
+
+            data.Save(@"d:\tetetete.xml");
+            */
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"E:\PracaInzynierska\PracaInzynierska\PracaInzynierska\Data\sampleXML_1.xml");
+
+            XmlNodeList nl = doc.SelectNodes("SRX_coefficients");
+            XmlNode root = nl[0];
+
+            foreach (XmlNode node in root.ChildNodes)
+            {
+                if (node.OfType<XComment>()) 
+                {
+                    
+                }
+
+                string name = node.Name;
+                double value = Double.Parse(node.InnerText);
+                Console.WriteLine("name {0}, value {1}", name, node.InnerText);
+                node.InnerText = (value*2).ToString();
+                Console.WriteLine("name {0}, value {1}", name, node.InnerText);
+
+                
+            }
+        }
+
+
+    }
+}
---
+++ LinqToXML/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqToXML
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender  { get; set; }
+        public int TotalMarks { get; set; }
+
+        public static Student[] GetAllStudens()
+        {
+            Student[] students = new Student[4];
+
+            students[0] = new Student { Id = 102, Name = "Mark", Gender = "Male", TotalMarks = 800 };
+            students[1] = new Student { Id = 103, Name = "Rosy", Gender = "Female", TotalMarks = 850 };
+            students[2] = new Student { Id = 102, Name = "Pam", Gender = "Male", TotalMarks = 900 };
+            students[3] = new Student { Id = 102, Name = "John", Gender = "Female", TotalMarks = 950 };
+
+            return students;
+        }
+
+    }
+}
